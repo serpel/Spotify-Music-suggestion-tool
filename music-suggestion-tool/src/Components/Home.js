49e8e1c8c484664bb89d90c5e0d8263c55bc1517
Serpel <serpel.js@gmail.com>
2018-10-17 14:34:29 -0600

    added login design + home routing

---
+++ music-suggestion-tool/src/Components/Home.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button'
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import CategoryList from '@material-ui/icons/FormatListBulleted'
+import { Redirect } from 'react-router-dom';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    height: '100%'
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    //color: theme.palette.text.secondary,
+    padding: theme.spacing.unit * 1,
+    minWidth: 0, // So the Typography noWrap works
+  },
+  toolbar: theme.mixins.toolbar,
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+});
+
+class HomeComponent extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      loggedIn: sessionStorage.getItem('loggedin') === 'true',
+      content: [],
+    }
+  }
+
+
+  handleLogOutClick = () =>{
+    console.log("logout");
+    sessionStorage.setItem('loggedin', false);
+    this.setState({
+      loggedIn: false
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    if(this.state.loggedIn === false){
+      return <Redirect to='/login'/>;
+    }
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="absolute" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              Inplas
+            </Typography>
+            <Button color="inherit" onClick={this.handleLogOutClick}>LogOut</Button>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.toolbar} />
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <CategoryList />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+            { this.state.content }
+        </main>
+      </div>
+    )
+  }
+}
+
+HomeComponent.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(HomeComponent);
---
+++ music-suggestion-tool/src/Components/Login.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import SpotifyIcon from '../Images/SpotifyIcon.png'
+
+import axios from 'axios';
+import { Redirect } from  'react-router-dom';
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        //justifyContent: 'space-around',
+        overflow: 'hidden',
+        //backgroundColor: 'powderblue',
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing.unit * 2,
+        height: '100vh',
+    },
+    container: {
+        //backgroundColor: theme.palette.background.paper,
+    },
+    button: {
+        margin: theme.spacing.unit,
+    },
+    extendedIcon: {
+        marginRight: theme.spacing.unit,
+    },
+});
+
+class LoginComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loggedIn: sessionStorage.getItem('loggedin') === 'true',
+            alertMessage: false,
+        }
+    }
+
+    onLoginSubmitHandler = (event) => {
+        event.preventDefault();
+    }
+
+    render(){
+
+        const { classes } = this.props;
+
+        if(this.state.loggedIn) {
+            return <Redirect to='/home'/>;
+        }
+
+        return (
+            <Paper className={classes.root}>
+               <Grid container spacing={12}>
+                    <Grid item xs={4}>
+                    </Grid>
+                    <Grid item xs={1} sm container className={classes.container}>
+                        <form onSubmit={this.onLoginSubmitHandler}>
+                            <Button variant="contained" color="default" type="submit" className={classes.button}>
+                                <img src={SpotifyIcon} alt="spotify" className={classes.extendedIcon} />
+                                Login With Spotify
+                            </Button>
+                        </form>
+                    </Grid>
+                    <Grid item xs={4}>
+                    </Grid>
+                </Grid>
+            </Paper>
+        )
+    }
+}
+
+LoginComponent.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LoginComponent)
