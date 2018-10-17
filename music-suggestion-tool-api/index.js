05413dc6dae9124f6e1f744d04393edcb4d07e11
Serpel <serpel.js@gmail.com>
2018-10-17 16:03:19 -0600

    Added spotify required package in the api

--- music-suggestion-tool-api/index.js
+++ music-suggestion-tool-api/index.js
@@ -1,8 +1,20 @@
+var dotenv = require('dotenv');
 var express = require('express');
+var request = require('request'); // "Request" library
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
 
+const result = dotenv.config();
 var app = express();
 
-var server = app.listen(8090, function(){
+if (result.error) {
+    throw result.error
+}
+
+console.log(result.parsed)
+
+var server = app.listen(8888, function(){
     console.log(`server running at - ${server.address().address}:${server.address().port}`);
 })
 
