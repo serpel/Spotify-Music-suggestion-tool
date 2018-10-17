d7bcbc2f1156777097f602460926abe7b20b32f8
Serpel <serpel.js@gmail.com>
2018-10-17 15:02:19 -0600

    playlist render body

--- music-suggestion-tool/src/Components/Playlist.js
+++ music-suggestion-tool/src/Components/Playlist.js
@@ -12,7 +12,13 @@ const styles = theme => ({
         //backgroundColor: 'powderblue',
         backgroundColor: theme.palette.background.paper,
         padding: theme.spacing.unit * 2,
-    }
+    },
+    header: {
+        
+    },  
+    playlist: {
+
+    },
 });
 
 
@@ -43,6 +49,36 @@ class PlaylistComponent extends React.Component {
 
     render() {
         const { classes } = this.props;
+
+        return (
+            <Paper className={classes.root}>
+                <Table className={classes.playlist}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell numeric>Img</TableCell>
+                            <TableCell>Tittle</TableCell>
+                            <TableCell numeric>Album</TableCell>
+                            <TableCell>Options</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {items.map(row => {
+                        return (
+                        <TableRow key={row.id}>
+                            <TableCell numeric>{row.Tittle}</TableCell>
+                            <TableCell component="th" scope="row">{row.Album}</TableCell>
+                            <TableCell numeric>
+                                <Buttom>
+                                    Play
+                                </Buttom>
+                            </TableCell>
+                        </TableRow>
+                        );
+                    })}
+                    </TableBody>
+                </Table>
+        </Paper>
+        );
     }
 }
 
