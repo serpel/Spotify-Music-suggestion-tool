f40843d323aa95b9d0e5ac395f793e4e840e139b
Serpel <serpel.js@gmail.com>
2018-10-24 00:48:34 -0600

    Move to root folder

---
+++ src/Components/Playlist/CurrentPlaying.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import IconBotton from '@material-ui/core/IconButton';
+
+function CurrentPlaying(props){
+    const { classes, name, artist, image } = props;
+
+    return(
+        <div className={classes.card}>
+            <div className={classes.track}>
+                <Typography variant="title">
+                    {name}
+                </Typography>
+                <Typography variant="body1" >
+                    {artist}
+                </Typography>
+            </div>
+            <IconBotton>
+                <img
+                    image={image}
+                    alt={name}
+                />
+            </IconBotton>
+        </div>
+    )
+}
+
+export default CurrentPlaying;
